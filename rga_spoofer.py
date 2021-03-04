7421991ffbe2fd37202f85888ff61a15093f7f5f
Rose <26861054+Roachbones@users.noreply.github.com>
2021-03-04 12:49:35 -0500

    started working on live plots

---
+++ rga_spoofer.py
@@ -0,0 +1,20 @@
+# "generates" data like the rga does, for testing the live plotter.
+
+import os
+import time
+import random
+#from chamberplot import parse_scan
+ 
+real_scan_paths = [i for i in os.listdir("rga_data") if i.startswith("MassSpecData")]
+real_scan_paths.sort()
+
+for real_scan_path in real_scan_paths:
+    with open("rga_data/" + real_scan_path) as file:
+        real_lines = file.read()
+
+    with open("spoofed_rga_data/" + real_scan_path, "w") as file:
+        for real_line in real_lines:
+            file.write(real_line)
+            time.sleep(0.01 * random.random())
+        
+        
